ceedefdd5a07270f0df98961283ed45c1271ed4b
Ashton <ashton.cantu@outlook.com>
2024-05-20 16:51:48 -0600

    - Merged test branch to main - Changed Colors

--- setup.ps1
+++ setup.ps1
@@ -21,19 +21,19 @@ winget uninstall --id "{35ED3F83-4BDC-4c44-8EC6-6A8301C7413A}" -h
 winget uninstall --id "MSC" -h 
 
 # Install Chrome
-Write-Host "Installing Google Chrome" -ForegroundColor Gray -BackgroundColor Blue
+Write-Host "Installing Google Chrome" -ForegroundColor Black -BackgroundColor Cyan
 winget install Google.Chrome -h --disable-interactivity --accept-package-agreements --accept-source-agreements
 
 # Install Reader
-Write-Host "Installing Adobe Reader" -ForegroundColor Gray -BackgroundColor Blue
+Write-Host "Installing Adobe Reader" -ForegroundColor Black -BackgroundColor Cyan
 winget install Adobe.Acrobat.Reader.64-bit -h --accept-package-agreements --accept-source-agreements
 
 # Install Citrix 
-Write-Host install "Citrix Workspace" -ForegroundColor Gray -BackgroundColor Blue
+Write-Host install "Citrix Workspace" -ForegroundColor Black -BackgroundColor Cyan
 winget install XPFCG3278HX4X9 -h --accept-package-agreements --accept-source-agreements
 
 # Install O365 App & Office Suite
-Write-Host install "Installing Office 365" -ForegroundColor Gray -BackgroundColor Blue
+Write-Host install "Installing Office 365" -ForegroundColor Black -BackgroundColor Cyan
 winget install 9WZDNCRD29V9 -h --accept-package-agreements --accept-source-agreements
 winget install Microsoft.Office -h --accept-package-agreements --disable-interactivity 
 
