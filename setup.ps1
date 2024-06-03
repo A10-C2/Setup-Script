ac37aceb161cf41fe2ed7bc682faca303a3fe37e
Ashton <ashton.cantu@outlook.com>
2024-06-03 12:13:33 -0600

    Added install/unistall lists

--- setup.ps1
+++ setup.ps1
@@ -67,31 +67,60 @@ function Set-Tz {
     }
 }
 
+function remove-pkgs {
+    $mcafeepkgs = @(
+            "McAfee.WPS"
+            "McAfeeWPSSparsePackage_0j6k21vdgrmfw" 
+            "{35ED3F83-4BDC-4c44-8EC6-6A8301C7413A}"
+            "MSC"
+            )
+        foreach ($package in mcafeepkgs) {
+        try {
+            Write-Host "Attempting to uninstall package: $package"
+                $result = winget uninstall --id  $package -h
+                if ($result -match "Successfully uninstalled") {
+                    Write-Host "Successfully uninstalled package: $package"
+                } else {
+                    Write-Host "Unistalllation of package may not have succeeded" -ForegroundColor Yellow
+                }
+        } catch {
+            Write-Host "Failed to unistall $package: $_" -ForegroundColor Red
+        }
+    }
+}
+
 function Install-Software {
     Write-Host "===== Installing Software =====" -ForegroundColor DarkBlue
-    # Uninstall McAfee
-    Write-Host "Uninstalling McAfee" -ForegroundColor Black -BackgroundColor Cyan
-    winget uninstall McAfee.WPS -h  
-    winget uninstall McAfeeWPSSparsePackage_0j6k21vdgrmfw -h 
-    winget uninstall --id "{35ED3F83-4BDC-4c44-8EC6-6A8301C7413A}" -h 
-    winget uninstall --id "MSC" -h 
+
+    $pkgs = @(
+        "Google.Chrome"
+        "Adobe.Acrobat.Reader.64-bit"
+        "XPFCG3278HX4X9"
+        "9WZDNCRD29V9"
+        "Mircosoft.Office"
+    )
+
+    foreach ($package in pkgs) {
+            Write-Host "Installing package: $package"
+            $result = winget install --id $package -h --accept-package-agreements --accept-source-agreements
+        }
 
     # Install Chrome
     Write-Host "Installing Google Chrome" -ForegroundColor Black -BackgroundColor Cyan
-    winget install Google.Chrome -h --disable-interactivity --accept-package-agreements --accept-source-agreements
+    # winget install Google.Chrome -h --disable-interactivity --accept-package-agreements --accept-source-agreements
 
     # Install Reader
     Write-Host "Installing Adobe Reader" -ForegroundColor Black -BackgroundColor Cyan
-    winget install Adobe.Acrobat.Reader.64-bit -h --accept-package-agreements --accept-source-agreements
+    # winget install Adobe.Acrobat.Reader.64-bit -h --accept-package-agreements --accept-source-agreements
 
     # Install Citrix 
     Write-Host install "Citrix Workspace" -ForegroundColor Black -BackgroundColor Cyan
-    winget install XPFCG3278HX4X9 -h --accept-package-agreements --accept-source-agreements
+    # winget install XPFCG3278HX4X9 -h --accept-package-agreements --accept-source-agreements
 
     # Install O365 App & Office Suite
     Write-Host install "Installing Office 365" -ForegroundColor Black -BackgroundColor Cyan
-    winget install 9WZDNCRD29V9 -h --accept-package-agreements --accept-source-agreements
-    winget install Microsoft.Office -h --accept-package-agreements --disable-interactivity 
+    # winget install 9WZDNCRD29V9 -h --accept-package-agreements --accept-source-agreements
+    # winget install Microsoft.Office -h --accept-package-agreements --disable-interactivity 
 
     # Upgrade all installed pkgs
     winget install upgrade -h --accept-package-agreements --accept-source-agreements
@@ -101,6 +130,7 @@ function Install-Software {
 Install-Winget
 Set-Tz
 Get-Sys-Info
+MacAfee-Uninstall
 Install-Software
 
 
